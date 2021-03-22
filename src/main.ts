cb2aa249d9cd3760559ac96aacf56872bb72447f
lvliu <liulibzz@163.com>
2021-03-22 11:02:46 +0800

    init:项目初始化

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { setupAntd } from '@/plugins';
+
+const app = createApp(App);
+
+// 注册全局常用的ant-design-vue组件
+setupAntd(app);
+
+app.use(store).use(router).mount('#app');
